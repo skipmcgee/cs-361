3ccb679fda67bff485feadb08283368c1f3aecec
Skip McGee <david.mcgee@usmc.mil>
2024-07-21 18:34:44 -0600

    minor mods

--- strings.py
+++ strings.py
@@ -3,8 +3,8 @@
 
 class Strings:
     def __init__(self,):
-        self.user_welcome = "Welcome to the free flight weather forecast application! Press enter or 'Q' to exit at any time."
-        self.lat_prompt = "Please define the Latitude for the forecast?"
-        self.long_prompt = "Please define the Longitude for the forecast?"
-        self.model_prompt= "Please select from the following options: \n\tA) ICON Weather Forecast\n\tB) MBLUE Weather Forecast"
-        self.next_prompt = "Select which option you would like to do next: \n\tA) Change Location (Lat/Long entry required)\n\tB) Change Model (no entry required)\n\tC) Change Model and Location (start over)"
+        self.user_welcome = "Welcome to the free flight weather forecast application!\nThis application provides MBLUE and ICON weather forecast information.\nYou will need to provide the desired Latitude and Longitude.\nPress enter to exit at any time."
+        self.lat_prompt = "Please define the Latitude for the forecast, or press Enter to quit:"
+        self.long_prompt = "Please define the Longitude for the forecast, or press Enter to quit:"
+        self.model_prompt= "Please select from the following options, or press Enter to quit: \n\tA) ICON Weather Forecast\n\tB) MBLUE Weather Forecast"
+        self.next_prompt = "Select which option you would like to do next: \n\tA) Change Location (Lat/Long entry required)\n\tB) Change Model (no entry required)\n\tC) Change Model and Location (start over)\n\tQ) Quit"
